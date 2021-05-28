224f797bb4756793068ea4e7169ed9714c3314a5
gfederica <effegiudice@gmail.com>
2021-05-28 17:25:46 +0200

    Completato (title nested in hero)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png" alt="">
     <Hero/>
   </div>
 </template>
@@ -24,5 +25,12 @@ export default {
   color: #2c3e50;
   margin: auto;
   padding: 0;
+}
+img {
+  position: absolute;
+  z-index: 2;
+  left: 50%;
+  top: 50px;
+  transform: translate(-50%);
 }
 </style>
